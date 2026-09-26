963483c942194f90dea278d8e416565752bab151
aboodcs <aboodyahya411@gmail.com>
2026-09-26 14:31:19 +0000

    add the files

--- Tomcat/provision.sh
+++ Tomcat/provision.sh
@@ -2,21 +2,21 @@
 
 set -e  ## this is means if any command fails stop the script immediately
 
-echo "Updating packages..."
+echo "updating packages"
 sudo apt-get update
 
-echo "Installing Java 8..."
+echo "installing java"
 sudo apt-get install -y openjdk-8-jdk wget tar
 
-echo "Checking Java version..."
+echo "checking java version"
 java -version
 
-echo "Downloading Tomcat 9..."
+echo "downloading tomcat"
 cd /opt
 
 wget -q https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.122/bin/apache-tomcat-9.0.122.tar.gz
 
-echo "Extracting Tomcat..."
+echo "extracting tomcat"
 
 tar -xzf apache-tomcat-9.0.122.tar.gz
 
@@ -24,14 +24,14 @@ mv apache-tomcat-9.0.122 tomcat
 
 rm apache-tomcat-9.0.122.tar.gz
 
-echo "Changing Tomcat port to 7070..."
+echo "changing tomcat port to 7070"
 
 sed -i 's/port="8080"/port="7070"/' /opt/tomcat/conf/server.xml
 
-echo "Starting Tomcat..."
+echo "starting tomcat"
 
 chmod +x /opt/tomcat/bin/*.sh
 
 /opt/tomcat/bin/startup.sh
 
-echo "Tomcat installation completed"
+echo "tomcat installation completed"
